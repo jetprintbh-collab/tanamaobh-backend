6ddec555bcd44bda0ecb783de9b38a9e5d88469d
IFOODE <miltonjrbarbosa@gmail.com>
2026-04-14 06:11:13 -0300

    fix bcrypt error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 dotenv.config()
 
